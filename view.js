d3161a6dad919cc96adf27c417bdb362800fdd51
cs-joey <89863700+cs-joey@users.noreply.github.com>
2024-03-04 10:54:21 -0600

    Update view.js

--- view.js
+++ view.js
@@ -72,3 +72,5 @@ $('.permbutton').click( function( e ) {
 
 // ---- Assign unique ids to everything that doesn't have an ID ----
 $('#html-loc').find('*').uniqueId() 
+
+let permissionPrintOut = define_new_effective_permissions('perm_', true)
